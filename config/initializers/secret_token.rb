b36bcf98d8094d9ccb192fd1b4d530a7c009a166
Noah Shafi <noshaf@gmail.com>
2013-06-28 14:27:17 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InstaStubClientScrapper::Application.config.secret_token = 'c7cdb9e27ccd3456ccd2a9d86f767a9d5168685894706da6103b19896d2b834cf8e0ea3b19b88d8836a39d33ebdc0ebec04acc1779629c2af88c82ac39ad53ff'
