819690e7c7126c9f262a3e50313866d046c41c27
Noah Shafi <noshaf@gmail.com>
2013-06-29 12:15:16 -0700

    Successfully sent my first email

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => 'app16614032@heroku.com',
+  :password       => 'dozw0hck',
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
 # Initialize the rails application
 InstaStubClientScrapper::Application.initialize!
